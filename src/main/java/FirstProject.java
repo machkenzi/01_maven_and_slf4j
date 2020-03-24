c7332d5ce07dbcb18f22662b2d615b104e82b8cc
armin <sifratz@hotmail.com>
2020-03-24 12:02:55 +0100

    created project with maven & added sl4j dependency

---
+++ src/main/java/FirstProject.java
@@ -0,0 +1,9 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstProject {
+    public static void main(String args[]) {
+        final Logger logger = LoggerFactory.getLogger(FirstProject.class);
+        logger.info("First Project Log");
+    }
+}
