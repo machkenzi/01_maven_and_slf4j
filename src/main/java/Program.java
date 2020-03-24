7e0e183f7e471f8536afc295def8666ba21027b3
armin <sifratz@hotmail.com>
2020-03-24 20:24:53 +0100

    renamed FirstProject file to Program file

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,9 +1,9 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FirstProject {
+public class Program {
     public static void main(String args[]) {
-        final Logger logger = LoggerFactory.getLogger(FirstProject.class);
+        final Logger logger = LoggerFactory.getLogger(Program.class);
         logger.info("First Project Log");
     }
 }
