d08111dbe07d07e82af839a75edb778b79f32a1f
armin <sifratz@hotmail.com>
2020-03-27 12:56:50 +0100

    added slf4j back because logback is no longer needed. mocked logger and changed MessageLogger class so it accepts logger as parameter.

--- src/main/java/logger/MessageLogger.java
+++ src/main/java/logger/MessageLogger.java
@@ -4,7 +4,16 @@ import org.slf4j.LoggerFactory;
 import java.util.Objects;
 
 public class MessageLogger {
-    private final Logger logger = LoggerFactory.getLogger(MessageLogger.class);
+
+    private final Logger logger;
+
+    public MessageLogger() {
+        this(LoggerFactory.getLogger(MessageLogger.class));
+    }
+
+    public MessageLogger(Logger logger) {
+        this.logger = logger;
+    }
 
     public void LogToStringToConsole(String message) {
         Objects.requireNonNull(message, "string must not be null");
